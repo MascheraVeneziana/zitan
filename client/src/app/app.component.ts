fd5df386fd6e5e7d8412b89e5b295fe280736bb8
elphe27 <saber777@hotmail.co.jp>
2018-02-07 17:31:29 +0900

    Add GetVersion when first load

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppInfoService } from './service/app-info.service';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  constructor(private appInfoService: AppInfoService, private titleService: Title) { }
+
+  ngOnInit() {
+    this.appInfoService.getVersion().subscribe(appName => {
+      localStorage.setItem('appName', appName.text());
+      this.titleService.setTitle(appName.text());
+    },
+      error => {
+        console.log(error);
+      });
+  }
+
 }

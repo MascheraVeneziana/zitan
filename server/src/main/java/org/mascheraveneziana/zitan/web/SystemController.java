a80cba471d3f8e56f94ce5c5e5953b2054964e5b
ishizuka <mizuho_ishizuka@unirita.co.jp>
2018-02-16 10:42:48 +0900

    アプリのバージョンを返すコントローラー

---
+++ server/src/main/java/org/mascheraveneziana/zitan/web/SystemController.java
@@ -0,0 +1,17 @@
+package org.mascheraveneziana.zitan.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/system")
+public class SystemController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String system() {
+        String str = "{ \"version\": \"1.0.0\" }";
+        return str;
+    }
+
+}

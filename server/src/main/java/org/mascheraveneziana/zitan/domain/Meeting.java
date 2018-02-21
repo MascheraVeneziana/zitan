190109d16962e61cb0f959aa766e4b71ea68924c
Taro937 <ttte8kv9@gmail.com>
2018-02-22 01:26:07 +0900

    ConferenceRoomからMeetingに変更、メンバー(userList)を追加

---
+++ server/src/main/java/org/mascheraveneziana/zitan/domain/Meeting.java
@@ -0,0 +1,29 @@
+package org.mascheraveneziana.zitan.domain;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Table(name="meeting")
+public class Meeting extends TimestampEntity{
+	
+	@Id
+	@GeneratedValue
+	@Getter @Setter
+	private Long id;
+	@Getter @Setter
+	private String name;
+	@Getter @Setter
+	private String room;
+	@Getter @Setter
+	List<User> user;
+	
+}
---
+++ server/src/main/java/org/mascheraveneziana/zitan/repository/MeetingRepository.java
@@ -0,0 +1,8 @@
+package org.mascheraveneziana.zitan.repository;
+
+import org.mascheraveneziana.zitan.domain.Meeting;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MeetingRepository extends CrudRepository<Meeting, Long> {
+
+}

84cf46faf18434cdf4866ee89efecd45a5e41f41
elphe27 <saber777@hotmail.co.jp>
2018-02-26 14:46:58 +0900

    追加　ダッシュボード　完成予定レイアウト

--- client/src/app/component/dashboard/dashboard.component.ts
+++ client/src/app/component/dashboard/dashboard.component.ts
@@ -18,7 +18,9 @@ export class DashboardComponent implements OnInit, AfterViewInit {
   public ELEMENT_DATA: Member[] = [];
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatTable) table: MatTable<any>;
-  @ViewChild('myCanvas') myCanvas;
+  @ViewChild('mtgDateCanvas') mtgDateCanvas;
+  @ViewChild('mtgCircleGraph') mtgCircleGraph;
+  @ViewChild('mtgOvertimeCanvas') mtgOvertimeCanvas;
 
   constructor() {
     this.memberType = Constants.memberType;
@@ -29,11 +31,15 @@ export class DashboardComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    const ctx = this.myCanvas.nativeElement;
-    this.drawChart(ctx);
+    const ctx = this.mtgDateCanvas.nativeElement;
+    this.drawDate(ctx);
+    const ctx2 = this.mtgCircleGraph.nativeElement;
+    this.drawCircle(ctx2);
+    const ctx3 = this.mtgOvertimeCanvas.nativeElement;
+    this.drawOvertime(ctx3);
   }
 
-  drawChart(ctx) {
+  drawDate(ctx) {
     return new Chart(ctx, {
       type: 'line',
       data: {
@@ -82,5 +88,80 @@ export class DashboardComponent implements OnInit, AfterViewInit {
     });
 
   }
+
+  drawOvertime(ctx) {
+    return new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['3/1','3/2','3/5','3/6','3/7','3/8','3/9','3/12','3/13','3/14','3/15','3/16','3/19','3/20','3/22','3/23','3/26','3/27','3/28','3/29','3/30','3/31'],
+        datasets: [{
+          label: '残業時間',
+          data: [1,2,3,4,5,6,7,8,9,10 ],
+          backgroundColor: 'rgba(101,107,255,0.2)',
+          borderColor: 'rgba(101,107,255,1)',
+          borderWidth: 2,
+          pointBackgroundColor: 'rgba(101,107,255,1)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(101,107,255,1)'
+        }, {
+          label: 'AI予測',
+          data: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22],
+          backgroundColor: 'rgba(200,150,22,0.2)',
+          borderColor: 'rgba(200,150,22,1)',
+          borderWidth: 2,
+          pointBackgroundColor: 'rgba(200,150,22,1)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(200,150,22,1)'
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+
+  }
+
+
+  drawCircle(ctx) {
+    return new Chart(ctx, {
+      type: 'pie',
+      data: {
+        datasets: [{
+          data: [
+            10,
+            20,
+            30,
+            20,
+          ],
+          backgroundColor: [
+            'red',
+            'orange',
+            'yellow',
+            'green',
+          ],
+          label: 'Dataset 1'
+        }],
+        labels: [
+          '会議超過あり：残業あり',
+          '会議超過あり：残業なし',
+          '会議超過なし：残業あり',
+          '会議超過なし：残業なし'
+        ]
+      },
+      options: {
+        responsive: true
+      }
+    });
+
+  }
+
 
 }

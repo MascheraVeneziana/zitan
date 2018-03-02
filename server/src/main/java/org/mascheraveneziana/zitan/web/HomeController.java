b8af9899c2b0c0c169e4ded061a0fc8f5d1743bf
ishizuka <mizuho_ishizuka@unirita.co.jp>
2018-03-02 18:43:20 +0900

    Google APIお試し（エラー起こりますが、処理は継続されます）

--- server/src/main/java/org/mascheraveneziana/zitan/web/HomeController.java
+++ server/src/main/java/org/mascheraveneziana/zitan/web/HomeController.java
@@ -1,5 +1,9 @@
 package org.mascheraveneziana.zitan.web;
 
+import java.io.IOException;
+import java.util.Map;
+
+import org.mascheraveneziana.zitan.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
@@ -7,8 +11,15 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.people.v1.PeopleService;
+import com.google.api.services.people.v1.model.Person;
 
 @RestController
 public class HomeController {
@@ -19,15 +30,29 @@ public class HomeController {
     ClientRegistrationRepository clientRegistrationRepository;
 
     @GetMapping(path = "/")
-    public String home(OAuth2AuthenticationToken authentication) {
+    public User home(OAuth2AuthenticationToken authentication) {
         OAuth2AuthorizedClient authorizedClient =
                 this.authorizedClientService.loadAuthorizedClient(
                         authentication.getAuthorizedClientRegistrationId(),
                         authentication.getName());
+        GoogleCredential credential = new GoogleCredential();
+        credential.setAccessToken(authorizedClient.getAccessToken().getTokenValue());
 
-        OAuth2AccessToken accessToken = authorizedClient.getAccessToken();
+        // TODO: 動きません。"People API"にプロジェクトを登録する必要があるというエラーが発生する。
+        PeopleService peopleService = new PeopleService.Builder(new NetHttpTransport(), new JacksonFactory(), credential).build();
+        System.out.println(peopleService.getServicePath());
+        try {
+            Person person = peopleService.people().get("people/me").setPersonFields("names,emailAddress").execute();
+            System.out.println(person.getNames());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        return "{\"token\":\"" + accessToken.getTokenValue() + "\"}";
+        Map<String, Object> map = authentication.getPrincipal().getAttributes();
+        User user = new User();
+        user.setName((String) map.get("name"));
+        user.setEmail((String) map.get("email"));
+        return user;
     }
 
 }

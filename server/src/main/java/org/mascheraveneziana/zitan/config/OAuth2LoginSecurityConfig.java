550b8df136515a1344b68b1de2003b6088fb87dc
Taro937 <ttte8kv9@gmail.com>
2018-03-06 08:57:23 +0900

    COnfigurationクラスに@Configurationを付与

--- server/src/main/java/org/mascheraveneziana/zitan/config/OAuth2LoginSecurityConfig.java
+++ server/src/main/java/org/mascheraveneziana/zitan/config/OAuth2LoginSecurityConfig.java
@@ -1,8 +1,10 @@
 package org.mascheraveneziana.zitan.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 public class OAuth2LoginSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override

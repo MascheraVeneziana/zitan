778f128f63249471b166106ac3e1a2f2299fb128
Taro937 <ttte8kv9@gmail.com>
2018-04-27 16:44:44 +0900

    LocalDateからDate,LocalTimeからTimeへの変更

--- server/src/main/java/org/mascheraveneziana/zitan/dto/MeetingDto.java
+++ server/src/main/java/org/mascheraveneziana/zitan/dto/MeetingDto.java
@@ -2,8 +2,7 @@ package org.mascheraveneziana.zitan.dto;
 
 import java.sql.Date;
 import java.sql.Time;
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -22,13 +21,15 @@ public class MeetingDto {
 	private Date date;
 	private Time startTime;
 	private Time endTime;
+	private List<UserDto> userList;
 	
-	public MeetingDto (String name, String room, Date date, Time startTime, Time endTime) {
+	public MeetingDto (String name, String room, Date date, Time startTime, Time endTime, List<UserDto> userList) {
 		this.name = name;
 		this.room = room;
 		this.date = date;
 		this.startTime = startTime;
 		this.endTime = endTime;
+		this.userList = userList; 
 	}
 	
 	public MeetingDto () {}
